4a3381981e0c55064c8e513053581eeb0f7f6128
Rich Patterson <rhp195@verizon.net>
2013-09-02 19:17:27 -0400

    Added another button and view to highlight issue.

--- apps/cont_view_problem/resources/main_page.js
+++ apps/cont_view_problem/resources/main_page.js
@@ -14,20 +14,26 @@ ContViewProblem.ViewMe2 = SC.LabelView.extend({
   value: "View #2"
 });
 
+ContViewProblem.ViewMe3 = SC.LabelView.extend({
+  layout: { left: 5, top: 60, width: 300, height: 24 },
+  value: "View #3"
+});
+
 // This page describes the main user interface for your application.
 ContViewProblem.mainPage = SC.Page.design({
   mainPane: SC.MainPane.design({
-    childViews: ['contView', 'butView'],
+    childViews: ['contView', 'butView2', 'butView3'],
     
     contView: SC.ContainerView.design({
+      backgroundColor: 'yellow',
       layout: { left: 0, top: 50, right: 0, bottom: 0 },
       nowShowing: null,
       contentView: ContViewProblem.ViewMe1.design()
     }),
     
-    butView: SC.ButtonView.design({
+    butView2: SC.ButtonView.design({
       layout: { top: 0, left:0, width: 100, height: 30 },
-      title: "Change View",
+      title: "Get View #2",
       
       mouseUp: function(evt)
       {
@@ -35,6 +41,21 @@ ContViewProblem.mainPage = SC.Page.design({
         
         if (contView)
           contView.set('nowShowing', 'ContViewProblem.ViewMe2');
+        else
+          console.log("contView is null.");
+      }
+    }),
+    
+    butView3: SC.ButtonView.design({
+      layout: { top: 0, left:110, width: 100, height: 30 },
+      title: "Get View #3",
+      
+      mouseUp: function(evt)
+      {
+        var contView = ContViewProblem.getPath('mainPage.mainPane.contView');
+        
+        if (contView)
+          contView.set('nowShowing', 'ContViewProblem.ViewMe3');
         else
           console.log("contView is null.");
       }
